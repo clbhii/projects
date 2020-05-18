0effbf4d1a2728b9c351567ec65fca28f90376fd
clbhii <clbhii@163.com>
2020-05-18 16:58:53 +0800

    common

---
+++ common/src/main/java/com/cl/common/util/StringUtil.java
@@ -0,0 +1,15 @@
+package com.cl.common.util;
+
+/**
+ * by cl at 2020/5/18 0018
+ */
+public class StringUtil {
+
+    public static boolean isEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+
+    public static boolean notEmpty(String str) {
+        return !isEmpty(str);
+    }
+}
